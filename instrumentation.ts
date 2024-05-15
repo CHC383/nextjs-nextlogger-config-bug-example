bdeedf2d3a517f2356c5339251e2467a807e8261
Huacong Cai <huacong.cai@labscale.com>
2024-05-15 16:18:52 -0700

    docs: Empty main Branch to Use Issue Specific Branches

--- instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  // https://github.com/sainsburys-tech/next-logger/issues/13#issuecomment-1763535453
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    await require("pino");
-    await require("next-logger");
-  }
-}
