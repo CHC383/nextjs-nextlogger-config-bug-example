76cfb66e2f4dd7735aa439903a7dfa29de07da7d
Huacong Cai <huacong.cai@labscale.com>
2024-05-15 16:35:26 -0700

    build: Update Example for Turbopack Module Loading Bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  serverExternalPackages: ["next-logger"],
   experimental: {
     // https://github.com/sainsburys-tech/next-logger/issues/13#issuecomment-1763535453
     instrumentationHook: true, // For next-logger
